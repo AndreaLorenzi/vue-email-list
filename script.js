555bbb7e8b5ba605e1ed8974f2250293ef779bab
Your Name <you@example.com>
2023-05-16 15:05:32 +0200

    add link

---
+++ script.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+});
+app.mount('#app')
+
+
+
+
+
+
+
+// https://flynn.boolean.careers/exercises/api/random/mail
